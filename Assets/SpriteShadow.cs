851b2108c05028ea18d277b7f967a84ea4142abd
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-05-02 23:52:29 +0200

    Add alley and office scenes

--- Assets/SpriteShadow.cs
+++ Assets/SpriteShadow.cs
@@ -8,25 +8,41 @@ public class SpriteShadow : MonoBehaviour, ISerializationCallbackReceiver
     [SerializeField]
     private Material m_shadowMaterial;
 
+    [SerializeField]
+    private bool m_receiveShadow;
+
+    [SerializeField]
+    private bool m_castShadow;
+
     public void OnAfterDeserialize() {
     }
 
     public void OnBeforeSerialize() {
         SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-        foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
-            spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-            spriteRenderer.receiveShadows = true;
-            spriteRenderer.material = m_shadowMaterial;
+        if (m_receiveShadow && m_castShadow) {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                spriteRenderer.receiveShadows = true;
+                spriteRenderer.material = m_shadowMaterial;
+            }
+        } else if (m_receiveShadow) {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                spriteRenderer.receiveShadows = true;
+                spriteRenderer.material = m_shadowMaterial;
+            }
+        } else if (m_castShadow) {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                spriteRenderer.receiveShadows = false;
+                spriteRenderer.material = m_shadowMaterial;
+            }
+        } else {
+            foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
+                spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                spriteRenderer.receiveShadows = false;
+                spriteRenderer.material = m_shadowMaterial;
+            }
         }
     }
-
-    // Start is called before the first frame update
-    //void Start() {
-    //    SpriteRenderer[] spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
-    //    foreach (SpriteRenderer spriteRenderer in spriteRenderers) {
-    //        spriteRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-    //        spriteRenderer.receiveShadows = true;
-    //        spriteRenderer.material = m_shadowMaterial;
-    //    }
-    //}
 }
